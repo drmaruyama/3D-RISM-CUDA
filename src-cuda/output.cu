e1424895d8adf3ad73d3afc3f5b850607362b8f6
MARUYAMA Yutaka <maruyama@flynn.local>
2020-09-09 23:44:06 +0900

    Refined output.cu

--- src-cuda/output.cu
+++ src-cuda/output.cu
@@ -7,16 +7,7 @@ void RISM3D :: output() {
 
   transform(outlist.begin(), outlist.end(), outlist.begin(), ::tolower);
 
-  int flag = 0;
-  
-  if (outlist.find("m") != string::npos) flag += 0x01;
-  if (outlist.find("d") != string::npos) flag += 0x02;
-  if (outlist.find("c") != string::npos) flag += 0x04;
-  if (outlist.find("g") != string::npos) flag += 0x08;
-  if (outlist.find("h") != string::npos) flag += 0x10;
-  if (outlist.find("e") != string::npos) flag += 0x20;
-
-  if ((flag & 0x01) == 0x01) {
+  if (outlist.find("m") != string::npos) {
     double pmv = cal_pmv();
     double * xmu = new double[sv -> natv * 2];
     cal_exchem(xmu);
@@ -24,7 +15,7 @@ void RISM3D :: output() {
     delete[] xmu;
   }
 
-  if ((flag & 0x02) == 0x02) {
+  if (outlist.find("d") != string::npos) {
     double * du;
     du = new double[su -> num * 3];
     cal_grad(du);
@@ -32,21 +23,20 @@ void RISM3D :: output() {
     delete[] du;
   }
 
-  if ((flag & 0x04) == 0x04) {
+  if (outlist.find("c") != string::npos) {
     output_cuv();
   }
 
-  if ((flag & 0x08) == 0x08) {
+  if (outlist.find("g") != string::npos) {
     output_guv();
   }
 
-  if ((flag & 0x10) == 0x10) {
+  if (outlist.find("h") != string::npos) {
     output_huv();
   }
 
-  if ((flag & 0x20) == 0x20) {
+  if (outlist.find("e") != string::npos) {
     double euv = cal_euv();
     output_euv(euv);
   }
-
 } 

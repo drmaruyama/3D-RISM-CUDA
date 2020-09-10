c3107b99238b9ab576de7ae0106255f8b47fd080
MARUYAMA Yutaka <maruyama@flynn.local>
2020-09-10 10:30:18 +0900

    Refined Warp problem

--- src-cuda/cal_exdft.cu
+++ src-cuda/cal_exdft.cu
@@ -132,10 +132,15 @@ __global__ void sum(double * ds, double2 * dhuv) {
   if (threadIdx.x < 32) {
     volatile double *smem = sdata;
     smem[threadIdx.x] += smem[threadIdx.x + 32];
+    __syncwarp();
     smem[threadIdx.x] += smem[threadIdx.x + 16];
+    __syncwarp();
     smem[threadIdx.x] += smem[threadIdx.x + 8];
+    __syncwarp();
     smem[threadIdx.x] += smem[threadIdx.x + 4];
+    __syncwarp();
     smem[threadIdx.x] += smem[threadIdx.x + 2];
+    __syncwarp();
     smem[threadIdx.x] += smem[threadIdx.x + 1];
   }
   if (threadIdx.x == 0) ds[blockIdx.x + blockIdx.y * gridDim.x] = sdata[0];
@@ -159,10 +164,15 @@ __global__ void sum2(double * ds, double2 * dwork, double2 * dhuv) {
   if (threadIdx.x < 32) {
     volatile double *smem = sdata;
     smem[threadIdx.x] += smem[threadIdx.x + 32];
+    __syncwarp();
     smem[threadIdx.x] += smem[threadIdx.x + 16];
+    __syncwarp();
     smem[threadIdx.x] += smem[threadIdx.x + 8];
+    __syncwarp();
     smem[threadIdx.x] += smem[threadIdx.x + 4];
+    __syncwarp();
     smem[threadIdx.x] += smem[threadIdx.x + 2];
+    __syncwarp();
     smem[threadIdx.x] += smem[threadIdx.x + 1];
   }
   if (threadIdx.x == 0) ds[blockIdx.x + blockIdx.y * gridDim.x] = sdata[0];
@@ -257,10 +267,15 @@ __global__ void cal_eda(double * ds, const double2 * __restrict__ dwork,
   if (threadIdx.x < 32) {
     volatile double *smem = sdata;
     smem[threadIdx.x] += smem[threadIdx.x + 32];
+    __syncwarp();
     smem[threadIdx.x] += smem[threadIdx.x + 16];
+    __syncwarp();
     smem[threadIdx.x] += smem[threadIdx.x + 8];
+    __syncwarp();
     smem[threadIdx.x] += smem[threadIdx.x + 4];
+    __syncwarp();
     smem[threadIdx.x] += smem[threadIdx.x + 2];
+    __syncwarp();
     smem[threadIdx.x] += smem[threadIdx.x + 1];
   }
   if (threadIdx.x == 0) ds[blockIdx.x + blockIdx.y * gridDim.x] = sdata[0];

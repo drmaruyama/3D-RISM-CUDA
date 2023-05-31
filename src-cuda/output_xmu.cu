a4a4bb3b8e08a652362e1122007e0e8bdb66b971
Yutaka Maruyama <drmaruyama@gmail.com>
2023-05-31 10:47:23 +0900

    Supported name list in output_xmu.cu

--- src-cuda/output_xmu.cu
+++ src-cuda/output_xmu.cu
@@ -23,30 +23,30 @@ void RISM3D :: output_xmu(double * & xmu, double pmv, double pressure) {
 
   double pcterm = - pressure * pmv * ibeta;
 
-  out_file << "Solvation_Free_Energy(SC): " << fixed << setprecision(5) 
-  	   << ibeta * xmua << " (kcal/mol)" << endl;
+  out_file << "SFE(SC)= " << fixed << setprecision(5) 
+  	   << ibeta * xmua << " !(kcal/mol)" << endl;
   for (int iv = 0; iv < sv -> natv; ++iv) {
-    out_file << "  " << iv << " -----> " << fixed << setprecision(5) 
+    out_file << "  " << iv << "= " << fixed << setprecision(5) 
              << ibeta * xmu[iv] << endl;
   }
   out_file << endl;
 
-  out_file << "Solvation_Free_Energy(GF): " << fixed << setprecision(5) 
-	   << ibeta * gf << " (kcal/mol)" << endl;
+  out_file << "SFE(GF)= " << fixed << setprecision(5) 
+	   << ibeta * gf << " !(kcal/mol)" << endl;
   for (int iv = 0; iv < sv -> natv; ++iv) {
-    out_file << "  " << iv << " -----> " << ibeta * xmu[sv -> natv + iv] 
+    out_file << "  " << iv << "= " << ibeta * xmu[sv -> natv + iv] 
 	     << endl;
   }
   out_file << endl;
 
-  out_file << "PMV: " << fixed << setprecision(5)
-           << pmv << " (cc/mol)" << endl;
+  out_file << "PMV= " << fixed << setprecision(5)
+           << pmv << " !(cc/mol)" << endl;
 
-  out_file << "Pressure: " << fixed << setprecision(5)
-           << pressure << " (kcal/cc)" << endl;
+  out_file << "Pressure= " << fixed << setprecision(5)
+           << pressure << " !(kcal/cc)" << endl;
 
-  out_file << "Correction_Term: " << fixed << setprecision(5) 
-             << pcterm << " (kcal/mol)" << endl;
+  out_file << "Correction_Term= " << fixed << setprecision(5) 
+             << pcterm << " !(kcal/mol)" << endl;
 
   out_file.close();
 } 

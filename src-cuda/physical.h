be4b2301a21207f623ed4e72c18dd55ee50c0af4
Yutaka Maruyama <drmaruyama@gmail.com>
2023-07-02 20:26:09 +0900

    Changed physical constants

--- src-cuda/physical.h
+++ src-cuda/physical.h
@@ -2,19 +2,18 @@
 #define PHYSICAL_H
 
 // Avogadro constant  mol^(-1)
-const double avogadoro = 6.02214129e23;
+const double avogadoro = 6.02214076e23;
 
 // Bohr radius  x 10^(-10) m 
-const double bohr = 0.52917721092;
+const double bohr = 0.5291771500001;
 
 // Boltzmann constant  J k^(-1)
-const double boltzmann = 1.3806488e-23;
+const double boltzmann = 1.380649e-23;
 
 // Hartree  J
-const double hartree = 4.35974434e-18;
+const double hartree = 4.359744722207185e-18;
 
 // kcal to J
 const double kcal2J = 4184.0;
 
 #endif  // PHYSICAL_H
-
